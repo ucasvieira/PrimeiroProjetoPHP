fbc50619d5ca01f666cde0972c2dd42776e5f331
ucasvieira <102530569+ucasvieira@users.noreply.github.com>
2022-03-29 12:00:02 -0300

    Delete TestePessoa.php

--- TestePessoa.php
+++
@@ -1,8 +0,0 @@
-<?php //Inicializando o php
-require_once("Pessoa.class.php"); //requirindo um outro arquivo
-
-$pessoa = new Pessoa(); //atribuindo a classe
-$pessoa->setNome("Jeferson Roberto de Lima"); //atribuindo valor a variável encapsulada em outro arquivo
-echo $pessoa->getNome(); //exibindo na tela o valor da variável encapsulada
-
-//encerrando o php?>
